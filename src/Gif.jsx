ab2f1017a61f390affcca0a48ecd3f1b6fdda6a6
Joezhang <451188360@qq.com>
2023-05-26 10:53:23 +0800

    更新

--- src/Gif.jsx
+++ src/Gif.jsx
@@ -1,64 +1,49 @@
-import React, { useEffect, useState, } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import gifimage from "./images/gif.png";
 import _ from "lodash";
-import "./bqb.less";
+import "./gif.less";
 import createTextCanvas from "./text2canvas.js";
 import html2canvas from "html2canvas";
 
 const GifComp = () => {
   const [text, setText] = useState("JJDD");
+  const imgRef = useRef(null);
 
   useEffect(() => {
     let canvas = document.createElement("canvas");
+    // canvas.style=
     // 绘制文字环境
     let context = canvas.getContext("2d");
-    const img = new Image();
-    img.src = gifimage;
-    img.onload = function() {
-        // draw(this);   
-         context.drawImage(img,400,400);
-    document.getElementById('git1').appendChild(canvas)
+    imgRef.current.onload = function () {
+      context.drawImage(imgRef.current, 0, 0, 200, 200);
+      document.getElementById("git1").appendChild(canvas);
     };
-    console.log(gifimage)
-    // context.drawImage(img,10,10);
-    // document.getElementById('git1').appendChild(canvas)
-    
+    console.log(gifimage);
   }, []);
-
-//   const draw = () => {
-//     ctx.drawImage(img, 10, 10);
-//   }
-
-
-  const download = () => {
-    // setActive(false);
-    // html2canvas(document.getElementById("draw-content")).then(function (
-    //   canvas
-    // ) {
-    //   var link = document.createElement("a");
-    //   document.body.appendChild(link);
-    //   link.download = text + ".jpg";
-    //   link.href = canvas.toDataURL();
-    //   link.target = "_blank";
-    //   link.click();
-    //   setActive(true);
-    // });
-  };
 
   return (
     <>
-      <div className="draw-content" id="draw-content">
+      <p>canvas</p>
+      <div className="gif-content">
         <div
           id="git1"
-        //   style={{ background: `url(${img})`, height: "100%" }}
+          //   style={{ background: `url(${img})`, height: "100%" }}
         ></div>
       </div>
+      <p>原图</p>
+      <img
+        ref={imgRef}
+        src={gifimage}
+        width={300}
+        height={300}
+        //   style={{ display: "none" }}
+      />
       <textarea
         className="bqb_cls"
         onChange={(e) => setText(e.target.value)}
         value={text}
       />
-      <button onClick={download}>生成图片</button>
+      <button onClick={console.log}>生成图片</button>
     </>
   );
 };
