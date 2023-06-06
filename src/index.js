03db70b496bc20338914335fd27dc321184bd513
Joezhang <451188360@qq.com>
2023-06-06 10:43:09 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./styles.less";
+import "./font.css";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
--- src/lightSignGif.jsx
+++ src/lightSignGif.jsx
@@ -4,22 +4,33 @@ import _ from "lodash";
 import "./gif.less";
 import GIF from "gif.js";
 import { HUE } from "./constant.js";
-import 'context-filter-polyfill';
-
+import "context-filter-polyfill";
 
 const LightSignGif = () => {
-  const [text, setText] = useState("蔡徐坤");
+  const [text, setText] = useState("YYDS");
   const [leftText, setLeft] = useState("狂粉");
   const [rightText, setRight] = useState("死忠粉");
+
+  const [fontSize, setFontSize] = useState(40);
+  const [font, setFont] = useState("");
 
   const imgRef = useRef(null);
   const gifRef = useRef(null);
 
+  const skipFirst = useRef(null);
+
   useEffect(() => {
     imgRef.current.onload = function () {
       genGIF();
+      skipFirst.current = true;
     };
   }, []);
+
+  useEffect(() => {
+    if (skipFirst.current) {
+      genGIF();
+    }
+  }, [fontSize, font]);
 
   const genGIF = () => {
     var gif = new GIF({
@@ -35,7 +46,7 @@ const LightSignGif = () => {
       context.filter = `hue-rotate(${i}deg)`;
       context.drawImage(imgRef.current, 0, 0, 200, 200);
       //灯牌字
-      context.font = "45px sans-serif";
+      context.font = `${fontSize}px ${font || "sans-serif"}`;
       context.fillStyle = "white";
       context.fillStyle = "transform: rotate(10deg)";
       context.filter = `hue-rotate(${i}deg)`;
@@ -46,7 +57,7 @@ const LightSignGif = () => {
 
       //左右字
       context.rotate((-7 * Math.PI) / 180);
-      context.font = "16px sans-serif";
+      context.font = `16px ${font || "sans-serif"}`;
       context.fillText(leftText, 5, 190);
       context.fillText(rightText, 140, 190);
 
@@ -86,26 +97,53 @@ const LightSignGif = () => {
         height={465}
         style={{ display: "none" }}
       />
-      <p>灯牌</p>
+      <p>
+        灯牌 {fontSize}像素 {font}
+      </p>
       <input
         className="gif_input"
         onChange={(e) => setText(e.target.value)}
         value={text}
+        onBlur={genGIF}
       />
-      <p>下左字</p>
+      <div>
+        <select
+          name="font"
+          id="font_select"
+          onChange={(e) => {
+            setFont(e.target.value);
+          }}
+        >
+          <option value="">默认系统字体</option>
+          <option>Zpix</option>
+          <option>pingfang</option>
+        </select>
+        <button
+          style={{ margin: "0 20px" }}
+          onClick={() => setFontSize(fontSize - 1)}
+        >
+          -
+        </button>
+        <button onClick={() => setFontSize(fontSize + 1)}>+</button>
+      </div>
+
+      <p className="gif_input">下左字</p>
       <input
         className="gif_input"
         onChange={(e) => setLeft(e.target.value)}
         value={leftText}
+        onBlur={genGIF}
       />
+
       <p>下右字</p>
       <input
         className="gif_input"
         onChange={(e) => setRight(e.target.value)}
         value={rightText}
+        onBlur={genGIF}
       />
       <div></div>
-      <button onClick={genGIF}>预览</button>
+      {/* <button onClick={genGIF}>预览</button> */}
 
       <div className="line-through">长按图片保存或者点击👇🏻生成</div>
       <button onClick={download}>生成图片</button>
